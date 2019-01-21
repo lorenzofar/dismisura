046188c403aa36dc76be13f746f86742c10c9816
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-01-21 19:25:52 +0100

    Added prototypes for data handling and UI builder

--- public/app.js
+++ public/app.js
@@ -1,5 +1,16 @@
 const socket = io();
 
+socket.on("welcome", (data) => {
+    // the server should provide some data
+    // store them and use them randomly to build UI
+
+    // invoke UI builder
+})
+
 window.addEventListener("click", function (event) {
     socket.emit("click", null);
 });
+
+function buildUI(){
+    // here we start adding random stuff to the website
+}
