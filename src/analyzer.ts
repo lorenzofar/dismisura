3238e75bd04739668e0c56e2b2bda93d9b561881
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-01-23 23:15:38 +0100

    Get both total and daily connections numbers
    Added query to retrieve also the number of users
    that opened the website in the current day

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -29,14 +29,20 @@ export function trackUserClick(client: string) {
 }
 
 /* ===== CONNECTIONS STATS ===== */
-export function getConnectionNumber(callback: (count: number, err: boolean) => void) {
-    dbClient("connection").count()
+export function getConnectionNumber(callback: (total: number, today: number, err: boolean) => void) {
+    // get data about total number of connections and for daily quota
+    dbClient("connection")
+        .select(
+            dbClient.raw("COUNT(CASE WHEN True THEN userid ELSE NULL END) AS total"),
+            dbClient.raw("COUNT(CASE WHEN DATE_TRUNC('day', timestamp)=DATE_TRUNC('day', CURRENT_TIMESTAMP) THEN userid ELSE NULL END) AS today")
+        )
         .then(data => {
-            let connectionsCount = data[0].count;
-            callback(connectionsCount, false);
+            let totalConnections = data[0].total;
+            let dailyConnections = data[0].today;
+            callback(totalConnections, dailyConnections, false);
         })
         .catch((err) => {
             console.error(err);
-            callback(0, true);
+            callback(0, 0, true);
         });
 }
