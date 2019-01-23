0c23a20ee5a98e027e811773990c84e08d7195d5
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-01-23 19:24:19 +0100

    Add function to get total number of connections

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -27,3 +27,16 @@ export function trackUserClick(client: string) {
             console.error(err);
         });
 }
+
+/* ===== CONNECTIONS STATS ===== */
+export function getConnectionNumber(callback: (count: number, err: boolean) => void) {
+    dbClient("connection").count()
+        .then(data => {
+            let connectionsCount = data[0].count;
+            callback(connectionsCount, false);
+        })
+        .catch((err) => {
+            console.error(err);
+            callback(0, true);
+        });
+}
