651a3fe7aa02626deb77612b0d0fd0eac827ebbc
Lorenzo Farinelli <lorenzo.farinelli@outlook.it>
2019-01-29 16:54:24 +0100

    Provide total time spent on website

--- src/analyzer.ts
+++ src/analyzer.ts
@@ -4,6 +4,7 @@ import { IncomingHttpHeaders } from "http2";
 export interface Stats {
     totalConnections: number;
     dailyConnections: number;
+    totalTime: number;
     totalClicks: number;
     dailyClicks: number;
 }
@@ -59,21 +60,23 @@ export function trackUserClick(client: string) {
 }
 
 /* ===== CONNECTIONS STATS ===== */
-function getConnectionNumber(callback: (total: number, today: number, err: boolean) => void) {
+function getConnectionNumber(callback: (total: number, today: number, time: number,  err: boolean) => void) {
     // get data about total number of connections and for daily quota
     dbClient("connection")
-        .select(
+        .select(            
+            dbClient.raw("SUM(time) AS totalTime"),
             dbClient.raw("COUNT(CASE WHEN True THEN userid ELSE NULL END) AS total"),
             dbClient.raw("COUNT(CASE WHEN DATE_TRUNC('day', timestamp)=DATE_TRUNC('day', CURRENT_TIMESTAMP) THEN userid ELSE NULL END) AS today")
         )
         .then(data => {
             let totalConnections = data[0].total;
             let dailyConnections = data[0].today;
-            callback(totalConnections, dailyConnections, false);
+            let totalTime = data[0].totaltime;
+            callback(totalConnections, dailyConnections, totalTime, false);
         })
         .catch((err) => {
             console.error(err);
-            callback(0, 0, true);
+            callback(0, 0, 0, true);
         });
 }
 
@@ -94,12 +97,13 @@ export function getClicksNumber(callback: (total: number, today: number, err: bo
         });
 }
 
-export function getStats(callback: (stats: Stats)=>void){
-    getConnectionNumber((totalConn, dailyConn) => {
+export function getStats(callback: (stats: Stats) => void) {
+    getConnectionNumber((totalConn, dailyConn, totalTime) => {
         getClicksNumber((totalClicks, dailyClicks) => {
             let data: Stats = {
                 totalConnections: totalConn,
                 dailyConnections: dailyConn,
+                totalTime: totalTime,
                 totalClicks: totalClicks,
                 dailyClicks: dailyClicks
             }
